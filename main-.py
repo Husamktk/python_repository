16c4c2ebf168cb9a00fad8911d54a380a86f105f
Husamulhaq <husamulhaq691@gmail.com>
2024-11-13 11:27:28 -0800

    variables and datatyes

---
+++ main-.py
@@ -0,0 +1,44 @@
+name: str = "pakistan"
+
+#variable=name
+#datatype=string
+#variable value = pakistan
+
+
+print(name)      #print variable value 
+
+print (type(name))   # print data type of variable (name)
+
+print(id(name)) # print physical address in memory
+
+
+age : int = 21 
+  
+#variable  = age 
+#data type = integer
+#value = 21
+
+print(age)    #print 21
+
+print (type(age))   # int
+
+print(id(age))    #address in memory
+
+
+pi : float = 3.14    #floatvalue 3.14 
+
+print (pi)     #3.14
+
+print(type(pi))    # float
+
+print(id(pi))    #  address
+
+
+bool_variable: bool = True # Boolean Data Type
+
+print(type(bool_variable)) #print true
+
+print(bool_variable) #print bool
+
+print (id(bool_variable)) #print address in memory
+
